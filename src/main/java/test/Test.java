70677147a2400f03157cf92ba189c134bf7f326e
Mamadou <senemamadou1999@gmail.com>
2023-10-04 23:58:23 +0000

    ajout de nouveau test

--- src/main/java/test/Test.java
+++ src/main/java/test/Test.java
@@ -10,7 +10,7 @@ public class Test {
 	public static void main(String[] args) {
 		List <Utilisateur> l=new UtilisateurHome().getAllUser();
 		System.out.println(l.size());
-		for (int i=0;i<=l.size()-1;i++)
+		for (int i=0;i<l.size();i++) 
 		{
 			System.out.println(l.get(i).getIdUtilisateur() +" "+l.get(i).getPrenom());
 		}
