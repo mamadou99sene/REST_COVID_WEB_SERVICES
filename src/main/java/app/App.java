d4c467cf4054bf58f44a043517e7cdf393d29814
Mamadou <senemamadou1999@gmail.com>
2023-10-04 23:03:05 +0000

    ajout du code metier pour notre application

---
+++ src/main/java/app/App.java
@@ -0,0 +1,10 @@
+package app;
+
+import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.core.Application;
+@ApplicationPath("/rs")
+public class App extends Application{
+
+}
+ 
---
+++ src/main/java/test/Test.java
@@ -0,0 +1,20 @@
+package test;
+
+import java.util.List;
+
+import bdbeans.Utilisateur;
+import bdbeans.UtilisateurHome;
+
+public class Test {
+
+	public static void main(String[] args) {
+		List <Utilisateur> l=new UtilisateurHome().getAllUser();
+		System.out.println(l.size());
+		for (int i=0;i<=l.size()-1;i++)
+		{
+			System.out.println(l.get(i).getIdUtilisateur() +" "+l.get(i).getPrenom());
+		}
+
+	}
+
+}
