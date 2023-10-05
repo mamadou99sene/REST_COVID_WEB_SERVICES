f15b4e64f91205796e4a449a2a2705b0fa1cf35b
Mamadou <senemamadou1999@gmail.com>
2023-10-05 15:10:36 +0000

    ajout de nouveau test

--- src/main/java/app/App.java
+++ src/main/java/app/App.java
@@ -1,10 +1,9 @@
 package app;
 
 import jakarta.ws.rs.ApplicationPath;
-import jakarta.ws.rs.Path;
 import jakarta.ws.rs.core.Application;
 @ApplicationPath("/rs")
 public class App extends Application{
+	 
 
 }
- 
