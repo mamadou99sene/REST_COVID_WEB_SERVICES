b3e1c2e88ba2bd25e4d6640b33eddfd8b7de888d
Mamadou <senemamadou1999@gmail.com>
2023-10-11 15:54:29 +0000

    code de test pour renvoyer les rendez vous en donnant l'utilisateur

--- src/main/java/test/Test.java
+++ src/main/java/test/Test.java
@@ -2,17 +2,25 @@ package test;
 
 import java.util.List;
 
+import bdbeans.Rendezvous;
+import bdbeans.RendezvousHome;
 import bdbeans.Utilisateur;
 import bdbeans.UtilisateurHome;
 
 public class Test {
 
 	public static void main(String[] args) {
-		List <Utilisateur> l=new UtilisateurHome().getAllUser();
+		/*List <Utilisateur> l=new UtilisateurHome().getAllUser();
 		System.out.println(l.size());
 		for (int i=0;i<l.size();i++) 
 		{
 			System.out.println(l.get(i).getIdUtilisateur() +" "+l.get(i).getPrenom());
+		}*/
+		Utilisateur u=new UtilisateurHome().findUserByID(2);
+		List<Rendezvous> rv=new RendezvousHome().findRvByUtilisateur(u);
+		for (int i=0;i<rv.size();i++)
+		{
+			System.out.println(rv.get(i).getTyperendezvous());
 		}
 
 	}
